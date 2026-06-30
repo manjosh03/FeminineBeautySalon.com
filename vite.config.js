aeb7a5ee90b728637b832c0f529e7f579bb1afc2
Manjosh <130603620+manjosh03@users.noreply.github.com>
2026-06-29 23:41:56 -0700

    setting gh page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Femininebeautysalon.com/',
   plugins: [react(), tailwindcss()],
-  base: '/femininebeautysalon.com/',
+
 })
